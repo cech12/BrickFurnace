c984a2cdc25ec5608693301970b3a044165248ad
cech12 <cech12@gmail.com>
2025-11-06 17:33:31 +0100

    burn time (fuel efficiency) can now be configured #47

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/) and this project adheres to [this versioning scheme](https://gist.github.com/cech12/69319028e88c50349a6b044000a6607b).
 
+## [1.21.10-4.9.1.0] - 2025-11-11
+### Added
+- burn time (fuel efficiency) can now be configured (all loaders) (thanks to Phyne_ for the idea) #47
+
 ## [1.21.10-4.9.0.0] - 2025-11-06
 ### Changed
 - updated to Minecraft 1.21.10 (Fabric 0.138.0+1.21.10, NeoForge 21.10.48-beta, Forge 60.0.17)
--- common/src/main/java/de/cech12/brickfurnace/platform/services/IConfigHelper.java
+++ common/src/main/java/de/cech12/brickfurnace/platform/services/IConfigHelper.java
@@ -21,6 +21,15 @@ public interface IConfigHelper {
     /** Maximal value of the cook time factor option */
     double COOK_TIME_FACTOR_MAX = 100D;
 
+    /** Default value of the burn time factor option */
+    double BURN_TIME_FACTOR_DEFAULT = 1D;
+    /** Config description of the burn time factor option */
+    String BURN_TIME_FACTOR_DESCRIPTION = "Burn time factor (fuel efficiency) of all added brick furnaces in relation to corresponding vanilla furnaces. (i. e. 0.5 - half the time [less efficient], 1.0 same time [default], 2.0 twice the time [more efficient])";
+    /** Minimal value of the burn time factor option */
+    double BURN_TIME_FACTOR_MIN = 0D;
+    /** Maximal value of the burn time factor option */
+    double BURN_TIME_FACTOR_MAX = 100D;
+
     /** Default value of the recipe blocked list option */
     String RECIPE_BLOCKED_LIST_DEFAULT = "";
     /** Config description of the recipe blocked list option */
@@ -44,6 +53,13 @@ public interface IConfigHelper {
      * @return configured value of the cook time factor option
      */
     double getCookTimeFactor();
+
+    /**
+     * Gets the configured value of the burn time factor option.
+     *
+     * @return configured value of the burn time factor option
+     */
+    double getBurnTimeFactor();
 
     /**
      * Gets the configured value of the recipe blocked list option.
